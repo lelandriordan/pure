3c2addb480c14c64220e2fc68b660f69c254a72d
Leland Riordan <leland@lelandriordan.com>
2014-07-18 09:28:18 -0400

    Added comments title

--- comments.php
+++ comments.php
@@ -10,3 +10,16 @@ if ( post_password_required() ) {
 	return;
 }
 ?>
+<div id="comments" class="comments-area">
+	
+	<?php if ( have_comments() ) : ?>
+		<h1 class="comments-title">
+			<?php
+				printf( _nx( '1 comment on &ldquo;%2$s&rdquo;', '%1$s comments on &ldquo;%2$s&rdquo;', get_comments_number(), 'comments title', 'pure' ),
+					number_format_i18n( get_comments_number() ), '<span>' . get_the_title() . '</span>' );
+			?>
+		</h1>
+
+	<?php endif; // have_comments ?>
+
+</div>
