30f3bb0afe8024d20142b9238e9c85acce8a4e19
Leland Riordan <leland@lelandriordan.com>
2014-07-19 03:51:12 -0400

    Added comment template call

--- single.php
+++ single.php
@@ -9,6 +9,13 @@ get_header(); ?>
 
 			<?php get_template_part( 'content', 'single' ); ?>
 
+			<?php
+				// Load the comment template if they are open or if there is at least one comment
+				if ( comments_open() || '0' != get_comments_number() ) :
+					comments_template();
+				endif;
+			?>
+
 		<?php endwhile; ?>
 
 </main>
