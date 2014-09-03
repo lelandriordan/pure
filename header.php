ce9801dc526552b3cc55d115d3d6f3825305bed0
Leland Riordan <leland@lelandriordan.com>
2014-09-02 21:25:43 -0400

    Fixed title and schema.org markup

--- header.php
+++ header.php
@@ -9,7 +9,7 @@
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title itemprop="name"><?php wp_title( '|', true, 'right' ); ?></title>
+	<title><?php wp_title(''); ?></title>
 	<?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
