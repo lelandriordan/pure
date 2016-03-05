7e885d005f4421f52ae5850b294e8b12f2e705b5
Leland Riordan <leland@lelandriordan.com>
2016-03-04 20:42:09 -0500

    Added missing semi-colon

--- templates/content.php
+++ templates/content.php
@@ -7,7 +7,7 @@
 	<header>
 		<?php pure_post_thumbnail(); ?>
 		<?php the_title( sprintf( '<h1 class="entry-title" itemprop="name"><a href="%s" rel="bookmark" itemprop="url">', esc_url( get_permalink() ) ), '</a></h1>' ); ?>
-		<?php pure_meta() ?>
+		<?php pure_meta(); ?>
 	</header>
 	<div class="entry-content" itemprop="articleBody">
 		<?php the_content( __( 'Read More <span class="meta-nav">&rarr;</span>', 'pure' ) ); ?>
